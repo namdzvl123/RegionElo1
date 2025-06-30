ab2be71ab877890e5cddc960f9232549f67d3885
namdzvl123 <131749603+namdzvl123@users.noreply.github.com>
2025-06-30 14:42:57 +0700

    Update RegionElo.java

--- RegionElo/src/main/java/me/regionelo/RegionElo.java
+++ RegionElo/src/main/java/me/regionelo/RegionElo.java
@@ -1,1 +1,37 @@
-// Main plugin class
+package me.regionelo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RegionElo extends JavaPlugin {
+
+    private static RegionElo instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // Load config
+        saveDefaultConfig();
+
+        // Register commands
+        getCommand("elo").setExecutor(new EloCommand(this));
+        getCommand("elotop").setExecutor(new EloTopCommand(this));
+
+        // Register PAPI placeholder nếu có
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new EloPlaceholder(this).register();
+            getLogger().info("PlaceholderAPI hook enabled.");
+        }
+
+        getLogger().info("RegionElo đã bật thành công!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("RegionElo đã tắt!");
+    }
+
+    public static RegionElo getInstance() {
+        return instance;
+    }
+}
